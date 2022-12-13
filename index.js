bad4aa617705e9ae9a906c3848dddff55f4187f6
George Elkik <gelkik@gmail.com>
2022-12-12 21:59:16 -0500

    commit tree e659356094b046b562d8706e88e2a02b22061d5e

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+    const cat = [...cats,name];
+    return cat;
+}
+
+function prependCat(name){
+    const cat = [name,...cats];
+    return cat;
+}
+
+function removeLastCat(){
+    const cat = cats.slice(0,-1);
+    return cat
+}
+
+function removeFirstCat(){
+    const cat = cats.slice(1);
+    return cat
+}
+
+
+
+
